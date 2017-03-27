c774bc1cef800c62abb0940c7ec8bc0135576165
Johnjimy Som <johnjimy.som@student.csulb.edu>
2017-03-27 14:05:58 -0700

    Example using the scroll feature in jQuery
    scrollTop,
    $(window).click(function()
    .load(function() for an automated effect

---
+++ scroll.js
@@ -0,0 +1,11 @@
+<script>
+  $(window).click(function() {
+  $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+});
+//you can use .load(function() for an automated effect
+</script>
+<html>
+<a href="#catalog" target="" id="">
+
+<a name="catalog" id="catalog" href="#"> </a>
+</html>
